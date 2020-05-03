177f9a633c52db8d44c561da8e0e7b998973511e
eiiir <chrysanthemum49@hotmail.com>
2020-05-04 01:27:52 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,14 +1,13 @@
-const Player = {
-	changeJob: (job) => {
-		playerIconDom.src = `img/${job}.png`;
-		document.cookie = `job=${job}; max-age=2419200`;
-	},
-	getCurrentJob: () => {
-		return iconSelectDom.value;
-	}
-}
-
 const iconSelectDom = document.getElementById("playerIconSelect");
 iconSelectDom.addEventListener("change", (e) => Player.changeJob(e.target.value));
 
 const playerIconDom = document.getElementById("playerIcon");
+
+const Player = {
+	changeJob: (job) => {
+		playerIconDom.src = `img/${job}.png`;
+		Player.currentJob = job;
+		document.cookie = `job=${job}; max-age=2419200`;
+	},
+	currentJob: iconSelectDom.value
+}
